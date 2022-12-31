e6f0f002e3c1aa75899a497ae0a5af1fb179bed0
Keith Moon <keef@keefmoon.com>
2022-12-31 12:30:19 +0000

    Trying again

---
+++ site-generation/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "MoonBaseSoftware",
+    platforms: [.macOS(.v12)],
+    products: [
+        .executable(
+            name: "MoonBaseSoftware",
+            targets: ["MoonBaseSoftware"]
+        )
+    ],
+    dependencies: [
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.9.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "MoonBaseSoftware",
+            dependencies: ["Publish"],
+            path: "Sources"
+        )
+    ]
+)
